2df3e98795ed13ba8918516ec07b35efb995d66b
Marco Emilio Vazquez <maremivazquez@ufl.edu>
2019-09-07 16:55:55 -0400

    completed assignment

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -12,7 +12,7 @@ var fs = require('fs'),
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
-
+mongoose.connect(config.db.uri, {useNewUrlParser: true});
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
@@ -20,7 +20,13 @@ var fs = require('fs'),
 
   Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
  */
-
+fs.readFile('listings.json', 'utf8', function(err, data){
+  var listingData = JSON.parse(data);
+  listingData.entries.forEach(function(element){
+    new Listing(element).save();
+  });
+  console.log('entries submitted succeessfully!');
+});
 
 /*  
   Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that 
--- queries.js
+++ queries.js
@@ -1,6 +1,12 @@
 /* Add all the required libraries*/
+var fs = require('fs'),
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
+mongoose.connect(config.db.uri, {useNewUrlParser: true});
 
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
@@ -10,24 +16,55 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.findOne({ name: 'Library West' }, function(err, doc) {
+    if (err) throw err;
+    console.log(doc);
+  });
 };
+
 var removeCable = function() {
   /*
     Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
-   */
+   */ 
+  Listing.findOne({ code: 'CABL' }, function(err, doc) {
+    if (err) throw err;
+    if (doc){
+      doc.remove(function(err) {
+        if(err) throw err;
+        console.log(doc);
+      });
+    }
+  });
 };
 var updatePhelpsMemorial = function() {
   /*
     Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
-   */
+   */ 
+  Listing.findOne({ name: 'Phelps Laboratory' }, function(err, doc) {
+    if (err) throw err;
+    if (doc){
+      doc.address = "1953 Museum Rd, Gainesville, FL 32603, United States";
+      doc.save(function(err) {
+        if(err) throw err;
+        console.log(doc);
+      });
+    }
+    else{
+      console.log('cannot find!');
+    }	    
+  });
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+  Listing.find({}, function(err, docs){
+    if (err) throw err;
+    console.log(docs);
+  });
 };
 
 findLibraryWest();
